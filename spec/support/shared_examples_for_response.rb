804d49b31127fcb47588f3f4f952b0b39b5285f2
toshiwo <toshiwo@toshiwo.com>
2013-07-12 03:01:48 +0900

    change shared_examples to TopLevel
     * fix deprecated of rspec 2.14.x

--- spec/support/shared_examples_for_response.rb
+++ spec/support/shared_examples_for_response.rb
@@ -1,13 +1,9 @@
-describe "shared examples" do
+shared_examples_for "a response" do |attr_names|
 
-  shared_examples_for "a response" do |attr_names|
+  it { expect( subject ).to be_an_instance_of Hash }
+  it { expect( subject ).to include('requested') }
 
-    it { expect( subject ).to be_an_instance_of Hash }
-    it { expect( subject ).to include('requested') }
-
-    (attr_names || []).each do |attr_name|
-      it { expect( subject ).to include(attr_name) }
-    end
+  (attr_names || []).each do |attr_name|
+    it { expect( subject ).to include(attr_name) }
   end
-
 end
